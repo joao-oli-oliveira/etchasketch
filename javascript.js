9d62c26e6419066be22ca47344bef5e5adf12146
Oli <93283957+joao-oli-oliveira@users.noreply.github.com>
2024-12-18 17:07:32 +0000

    ALlow only to change background color on mousedown
    This way you can draw eyes and mouth yay

--- javascript.js
+++ javascript.js
@@ -1,11 +1,36 @@
 const board = document.getElementById("board");
+let isToggling = false;
+
+function enableToggle() {
+  console.log("enableToggle");
+  isToggling = true;
+}
+
+function disableToggle() {
+  console.log("disableToggle");
+  isToggling = false;
+}
 
 for (let i = 0; i < 100; i++) {
   const pixel = document.createElement("div");
   pixel.classList.add("pixel");
   pixel.style.minWidth = "60px";
   board.appendChild(pixel);
+  pixel.addEventListener("mousedown", enableToggle);
+  pixel.addEventListener("mouseup", disableToggle);
   pixel.addEventListener("mouseover", () => {
-    pixel.style.backgroundColor = "black";
+    if (isToggling === true) {
+      pixel.style.backgroundColor = "black";
+    } // Only turns background black if mousedown
   });
 } // Creates the pixels on the board
+
+// for (let i = 0; i < 100; i++) {
+//   const pixel = document.createElement("div");
+//   pixel.classList.add("pixel");
+//   pixel.style.minWidth = "60px";
+//   board.appendChild(pixel);
+//   pixel.addEventListener("mouseover", () => {
+//     pixel.style.backgroundColor = "black";
+//   });
+// } // Creates the pixels on the board
