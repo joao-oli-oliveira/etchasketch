a112d61c7a6b3b44f0cea72f883ed5ad5db7bd14
Oli <93283957+joao-oli-oliveira@users.noreply.github.com>
2024-12-19 15:49:40 +0000

    Adds a new resolution option and mouseclick color

--- javascript.js
+++ javascript.js
@@ -12,27 +12,36 @@ function disableToggle() {
   isToggling = false;
 }
 
+// for (let i = 0; i < 1024; i++) {
+//   const pixel = document.createElement("div");
+//   pixel.classList.add("pixel");
+//   pixel.style.minWidth = "18.75px";
+//   board.appendChild(pixel);
+//   pixel.addEventListener("mousedown", enableToggle);
+//   pixel.addEventListener("mouseup", disableToggle);
+//   pixel.addEventListener("mouseover", () => {
+//     if (isToggling === true) {
+//       pixel.style.backgroundColor = "rgb(64, 46, 23)";
+//       pixel.style.opacity -= "-0.1";
+//     } // Only turns background black if mousedown
+//   });
+// } // 32x32 pixels on the board
+
 for (let i = 0; i < 100; i++) {
   const pixel = document.createElement("div");
   pixel.classList.add("pixel");
   pixel.style.minWidth = "60px";
   board.appendChild(pixel);
   pixel.addEventListener("mousedown", enableToggle);
+  pixel.addEventListener("click", () => {
+    pixel.style.backgroundColor = "rgb(64, 46, 23)";
+    pixel.style.opacity -= "-0.1";
+  });
   pixel.addEventListener("mouseup", disableToggle);
   pixel.addEventListener("mouseover", () => {
     if (isToggling === true) {
-      pixel.style.backgroundColor = "black";
+      pixel.style.backgroundColor = "rgb(64, 46, 23)";
       pixel.style.opacity -= "-0.1";
     } // Only turns background black if mousedown
   });
-} // Creates the pixels on the board
-
-// for (let i = 0; i < 100; i++) {
-//   const pixel = document.createElement("div");
-//   pixel.classList.add("pixel");
-//   pixel.style.minWidth = "60px";
-//   board.appendChild(pixel);
-//   pixel.addEventListener("mouseover", () => {
-//     pixel.style.backgroundColor = "black";
-//   });
-// } // Creates the pixels on the board
+} // 10x10 pixels on the board
